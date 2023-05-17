d16ff2b789daefccd77992b6f65fa57c32429bfe
GOEUN PARK <dbtshfdl1@gmail.com>
2023-05-17 12:33:49 +0900

    5/17 수업 완료

--- src/Signup.js
+++ src/Signup.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, TextField, Link, Grid, Container, Typography } from "@material-ui/core";
+import { Button, TextField, Link, Grid, Container, Typography} from "@material-ui/core";
 
 import { signup } from "./service/ApiService";
 
@@ -9,12 +9,23 @@ class SignUp extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handlerSubmit(event) {
+    handleSubmit(event) {
+        event.preventDefault();
 
+        const data = new FormData(event.target);
+        const username = data.get("username");
+        const email = data.get("email");
+        const password = data.get("password");
+
+        signup({email:email, username:username, password:password}).then(
+            (response) => {
+                window.location.href = "/login";
+            }
+        );
     }
 
     render() {
-        return(
+        return (
             <Container component="main" maxWidth="xs" style={{ marginTop: "8%"}}>
                 <form noValidate onSubmit={this.handleSubmit}>
                     <Grid container spacing={2}>
@@ -23,55 +34,51 @@ class SignUp extends React.Component {
                                 계정 생성
                             </Typography>
                         </Grid>
-                        <TextField
-                        autoComplete="fname"
-                        name="username"
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="username"
-                        label="사용자 이름"
-                        autoFocus
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        id="email"
-                        label="이메일 주소"
-                        name="email"
-                        autoComplete="email"
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        name="이메일 주소"
-                        autoComplete="email"
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                        variant="outlined"
-                        required
-                        fullWidth
-                        name="password"
-                        label="패스워드"
-                        type="password"
-                        autoComplete="current-password"/>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary">
-                            계정 생성
-                        </Button>
+                        <Grid item xs={12}>
+                            <TextField
+                                autoComplete="fname"
+                                name="username"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="username"
+                                label="사용자 이름"
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                autoComplete="email"
+                                name="email"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                label="이메일 주소"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                name="password"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="password"
+                                type="password"
+                                label="패스워드"
+                                autoComplete="current-password"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                            >
+                                계정 생성
+                            </Button>
+                        </Grid>
                     </Grid>
                     <Grid container justify="flex-end">
                         <Grid item>
